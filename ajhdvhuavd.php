65084d1cc2af944fcfbf948611f28e4ffc5069f2
daimi106 <71595676+daimi106@users.noreply.github.com>
2025-10-07 10:03:52 +0200

    Add files via upload

---
+++ ajhdvhuavd.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="person-selector">
+     <button class="button person-selector-button active-person" id="john-selector">Jhon</button>
+          <button class="button person-selector-button" id="john-selector">Jhon</button>
+      </div>
+        <div class="chat-container"></div>
+        <h2 class="chat-header">jhon chating...</h2>
+               <div class="chat-messages"></div>
+                   <div class="massage blue-bg"> 
+                   <div class="message-sender">Jhon</div>
+                   <div class="message-text">hey Jane, whats up?</div>
+                   <div class="message-timestamp">10:30 AM</div>
+                </div> 
+                <div class="messages gray-bg">
+                    <div class="message-sender">Jane</div>
+                    <div class="message-text">.......</div>
+                    <div class="message-text">10:35</div>
+       </div>
+            <form class="chat-input-from">
+                <input type="text" class="chat-input" required placeholder="type here, jhon" />
+                <button type="submit"class="button send button">send</button>
+            </form>
+                <butto class="button" class="clear-chat-button">clear chat </button>
+   </body>
+  </html>
